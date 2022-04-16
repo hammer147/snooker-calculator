b1f5702209ce31451cade067e70422e49c1b454d
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-04-16 14:50:37 +0200

    added logic to remaining balls

--- components/balls-remaining.tsx
+++ components/balls-remaining.tsx
@@ -2,15 +2,38 @@ import NumberDisplay from './number-display'
 import NumberInput from './number-input'
 import styles from './balls-remaining.module.css'
 import Title from './title'
+import { useContext, useState } from 'react'
+import { AppContext } from '../context/app-context'
+import { adjustRemainingBalls, setRemainingBalls } from '../context/game'
 
 const BallsRemaining = () => {
+
+  const [number, setNumber] = useState(0) // number to modify remaining balls
+
+  const { state: { game }, dispatch } = useContext(AppContext)
+  const { numReds, numColors } = game
+
   return (
     <>
       <Title text='Balls Remaining' />
       <div className={styles.container}>
-        <NumberDisplay display="Reds" value={10} inputValue={1} />
-        <NumberInput />
-        <NumberDisplay display="Colors" value={6} inputValue={1} />
+        <NumberDisplay
+          display="Reds"
+          value={numReds}
+          inputValue={number}
+          subtract={(x: number) => dispatch(adjustRemainingBalls('numReds', -x))}
+          add={(x: number) => dispatch(adjustRemainingBalls('numReds', x))}
+          setTo={(x: number) => dispatch(setRemainingBalls('numReds', x))}
+        />
+        <NumberInput number={number} setNumber={setNumber} />
+        <NumberDisplay
+          display="Colors"
+          value={numColors}
+          inputValue={number}
+          subtract={(x: number) => dispatch(adjustRemainingBalls('numColors', -x))}
+          add={(x: number) => dispatch(adjustRemainingBalls('numColors', x))}
+          setTo={(x: number) => dispatch(setRemainingBalls('numColors', x))}
+        />
       </div>
     </>
   )
