1c82b4c73b461dc2fa74739b2d8987a229334560
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-04-17 10:55:11 +0200

    added logic to select next ball

--- components/next-ball.tsx
+++ components/next-ball.tsx
@@ -1,14 +1,38 @@
 import Title from './title'
 import styles from './next-ball.module.css'
+import { useContext } from 'react'
+import { AppContext } from '../context/app-context'
+import { setNextBall } from '../context/game'
 
 const NextBall = () => {
+
+  const { state: { game }, dispatch } = useContext(AppContext)
+  const { nextIsColorAfterRed, nextIsFreeBall } = game
+
   return (
     <>
       <Title text='Next Ball' />
       <div className={styles.container}>
-        <div>Red or Color</div>
-        <div>Color after Red</div>
-        <div>Free Ball</div>
+        <div
+          className={(!nextIsColorAfterRed && !nextIsFreeBall) ? styles.active : ''}
+          onClick={() => { dispatch(setNextBall('redOrLowestColor')) }}
+        >
+          Red or Color
+        </div>
+
+        <div
+          className={nextIsColorAfterRed ? styles.active : ''}
+          onClick={() => { dispatch(setNextBall('colorAfterRed')) }}
+        >
+          Color after Red
+        </div>
+
+        <div
+          className={nextIsFreeBall ? styles.active : ''}
+          onClick={() => { dispatch(setNextBall('freeBall')) }}
+        >
+          Free Ball
+        </div>
       </div>
     </>
   )
